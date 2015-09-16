dc9df908b8c02bc7ea08eff3541c715d360d4c71
Artur Gavkaliuk <mandeburka@gmail.com>
2015-09-16 16:52:46 +0300

    add distance test with scalamock

--- build.sbt
+++ build.sbt
@@ -5,5 +5,6 @@ version := "0.1alpha"
 scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq(
-  "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test"
+  "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test",
+  "org.scalamock" %% "scalamock-scalatest-support" % "3.2" % "test"
   )
--- src/test/scala/org/kih6/spy/rrb2/DistanceSpec.scala
+++ src/test/scala/org/kih6/spy/rrb2/DistanceSpec.scala
@@ -1,15 +1,34 @@
 package org.kih6.spy.rrb2
 
+import com.pi4j.io.gpio.{PinState, GpioPinDigitalOutput, GpioPinDigitalInput}
+import org.scalamock.scalatest.MockFactory
+import scala.concurrent.Await
 import scala.concurrent.duration._
-
 import org.scalatest.{Matchers, FlatSpec}
 
-class DistanceSpec extends FlatSpec with Matchers {
+class DistanceSpec extends FlatSpec with MockFactory with Matchers {
   "Zero time signal" should "have zero distance" in {
     Distance.nanosToDistance(0) should be (0)
   }
 
   "2 second signal" should "be equal to sound speed" in {
     Distance.nanosToDistance(2.second.toNanos) should be (Distance.SOUND_SPEED)
+  }
+
+  "Ultrasonic distance calculation" should "use trigger and echo pins" in {
+    val triggerPinMock = mock[GpioPinDigitalOutput]
+    val echoPinMock = mock[GpioPinDigitalInput]
+
+    val instance = new Distance {
+      val triggerPin: GpioPinDigitalOutput = triggerPinMock
+      val echoPin: GpioPinDigitalInput = echoPinMock
+    }
+    inSequence {
+      (triggerPinMock.high _).expects()
+      (triggerPinMock.low _).expects()
+      (echoPinMock.getState _).expects().returns(PinState.HIGH)
+      (echoPinMock.getState _).expects().returns(PinState.LOW)
+    }
+    Await.result(instance.distance, 1.second)
   }
 }
